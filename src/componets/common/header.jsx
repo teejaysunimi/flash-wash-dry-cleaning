ae8504bcdcd247de1ef79d5f0776e1558da1c4ab
Tijesunimi Emmanuel <tijesunimiemmanuel@gmail.com>
2025-10-04 07:59:32 +0100

    progress 1

---
+++ src/componets/common/header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Head from "./heading/Head"
+const header = () => {
+  return (
+      <div>
+          <Head/>
+    </div>
+  )
+}
+
+export default header
---
+++ src/componets/common/heading/Head.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Head = () => {
+  return (
+    <div>
+          <section className="head">
+              <div className="container flexSB">
+                  <div className="logo">
+                      <h1>FLASHWASH</h1>
+                  </div>
+              </div>
+        </section>
+    </div>
+  )
+}
+
+export default Head
